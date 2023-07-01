9d005c207f607dd895e6513099a8bfa37495cf08
Valdielson Siqueira <valdielson.silva@gmail.com>
2023-07-01 10:35:27 -0300

    feat(product): adicionado estilização da listagem de comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://avatars.githubusercontent.com/u/35588487?v=4"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Valdielson Siqueira</strong>
+              <time title="22 de Junho às 19:43" dateTime="2023-06-22 19:43:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir
+            <span> 20 </span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
