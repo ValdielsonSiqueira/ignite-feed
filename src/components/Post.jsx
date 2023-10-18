1fa2047ce242ae1ef2ea117e500bb2cdc9cace57
Valdielson Siqueira <valdielson.silva@gmail.com>
2023-10-18 20:48:27 -0300

    feature(product): map para renderizar comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -40,19 +40,13 @@ export function Post({ post: { author, content, publishedAt } }) {
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="">👉 jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a>
-          <a href="">#nlw</a>
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(line => { 
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p key={line.content}><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
       <form className={styles.commentForm} onSubmit={handlerCreateNewComment}>
         <strong>Deixe seu feedback!</strong>
